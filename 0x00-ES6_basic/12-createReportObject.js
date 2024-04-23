43b0fa770c99b0357ee7579120ee7ca9700f83b5
noahwekesa <noahwekesa0@gmail.com>
2024-04-23 15:12:33 +0300

    fix quotes

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,1 +1,10 @@
-
+export default function createIteratorObject(report) {
+  return {
+    allEmployees: {
+      ...report,
+    },
+    getNumberOfDepartments(empList) {
+      return Object.keys(empList).length;
+    },
+  };
+}
