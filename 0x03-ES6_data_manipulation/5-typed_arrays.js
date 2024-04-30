4c968fb97b74713da56fc2d110d90645793d6bd0
noahwekesa <noahwekesa0@gmail.com>
2024-04-30 14:31:45 +0300

    typed array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,15 +1,16 @@
-function createInt8TypedArray(length, position, value) {
-  if (position < 0 || position > length - 1) {
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+
+  // Create an Int8Array view of the buffer
+  const int8Array = new Int8Array(buffer);
+
+  // Check if the position is within the range of the buffer
+  if (position < 0 || position >= int8Array.length) {
     throw new Error('Position outside range');
   }
 
-  const buffer = new ArrayBuffer(length);
-
-  const int8Array = new Int8Array(buffer);
-
+  // Set the value at the specified position
   int8Array[position] = value;
 
-  return int8Array;
+  return buffer;
 }
-
-export default createInt8TypedArray;
