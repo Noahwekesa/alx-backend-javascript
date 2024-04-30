479c50390c127f571ebb6dcfb76097c3962063e9
noahwekesa <noahwekesa0@gmail.com>
2024-04-30 14:28:23 +0300

    combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,10 @@
-function updateStudentGradeByCity(students, city, newGrades) {
+export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
-    .filter((student) => student.city === city)
+    .filter((student) => student.location === city)
     .map((student) => {
       const foundGrade = newGrades.find(
         (grade) => grade.studentId === student.id,
       );
-      return {
-        ...student,
-        grade: foundGrade ? foundGrade.grade : 'N/A',
-      };
+      return { ...student, grade: foundGrade ? foundGrade.grade : 'N/A' };
     });
 }
