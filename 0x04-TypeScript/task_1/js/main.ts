6f1fa7f2feb5780ffd803f0e90a4f3d2dd436533
noahwekesa <noahwekesa0@gmail.com>
2024-05-02 15:54:15 +0300

    still facing errors

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,3 +15,31 @@ interface Directors extends Teacher {
 function printTeacher(firstName: string, lastName: string): string {
   return `${firstName.charAt(0)}. ${lastName}`;
 }
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+interface StudentClassConstructor {
+  new (fristName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework() {
+    return "Currently working";
+  }
+  displayName() {
+    return this.firstName;
+  }
+}
