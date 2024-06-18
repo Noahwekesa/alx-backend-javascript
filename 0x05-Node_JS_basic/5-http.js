7ab96b61145880ac319f82a28b1439a41b8cba60
noahwekesa <noahwekesa0@gmail.com>
2024-06-18 21:00:45 +0300

    express

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -10,33 +10,39 @@ const app = http.createServer((req, res) => {
       res.end('Hello Holberton School!');
       break;
     case '/students':
-      const filePath = process.argv[2];
+      const filePath = process.argv[2]; // Get database path from argument
       if (!filePath) {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end('Missing database file');
         return;
       }
-      fs.read(filePath, 'utf8', (err, data) => {
+
+      fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
-          res, writeHead(500, { 'Content-Type': 'text/plain' });
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
           res.end('Error reading database');
           return;
         }
-        const studentList = processStudents(data);
-        res.writeHead(100, { 'Content-Type': 'text/plain' });
-        res.end(`This is the list of out students\n ${studentList}`);
+
+        const studentList = processStudents(data); // Process student data
+
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(`This is the list of our students\n${studentList}`);
       });
       break;
     default:
       res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end('Not Found');
-      break;
+      res.end('Not found');
   }
 });
+
 function processStudents(data) {
-  const students = data.students.map((student) => student);
-  return students.join('\n');
+  // Implement logic to process student data (similar to 3-read_file_async.js)
+  // You can use techniques like split, filter, etc. to manipulate the data
+  // This function can return a formatted string representing the student list
+  return 'Student list processing logic goes here...'; // Replace with actual logic
 }
+
 app.listen(1245);
 
 module.exports = app;
