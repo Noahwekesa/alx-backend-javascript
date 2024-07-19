196438d05d5309831d9222c4be61d5f82b96fbaa
noahwekesa <noahwekesa0@gmail.com>
2024-07-19 22:48:07 +0300

    re

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,31 +1,43 @@
 const fs = require('fs');
 
-function countStudents(path) {
+function countStudents(database) {
+  let students = [];
+  const StudentGroup = {};
+  const studentObj = [];
+
   try {
-    const data = fs.readFileSync(path, { encoding: 'utf8' });
-    const lines = data.split('\n').filter((line) => line.length > 0);
-    let count = 0;
-    const fields = {};
-    for (const line of lines) {
-      count += 1;
-      const student = line.split(',');
-      if (fields[student[3]]) {
-        fields[student[3]].push(student[0]);
-      } else {
-        fields[student[3]] = [student[0]];
-      }
-    }
-    console.log(`Number of students: ${count}`);
-    for (const field in fields) {
-      if (field) {
-        const list = fields[field];
-        console.log(
-          `Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`,
-        );
-      }
-    }
-  } catch (err) {
+    students = fs.readFileSync(database, 'utf8');
+  } catch (error) {
     throw new Error('Cannot load the database');
+  }
+
+  students = students.split('\n');
+  const header = students.shift().split(',');
+
+  students.forEach((element) => {
+    if (element) {
+      const studentInfo = element.split(',');
+
+      header.forEach((header, index) => {
+        studentObj[header] = studentInfo[index];
+        if (header === 'field') {
+          if (StudentGroup[studentInfo[index]]) {
+            StudentGroup[studentInfo[index]].push(studentObj.firstname);
+          } else {
+            StudentGroup[studentInfo[index]] = [studentObj.firstname];
+          }
+        }
+      });
+      studentObj.push(studentObj);
+    }
+  });
+  console.log(`Number of students: ${studentObj.length}`);
+
+  for (const info in StudentGroup) {
+    if (StudentGroup[info]) {
+      const listStudents = StudentGroup[info];
+      console.log(`Number of students in ${info}: ${listStudents.length}. List: ${listStudents.join(', ')}`);
+    }
   }
 }
 
