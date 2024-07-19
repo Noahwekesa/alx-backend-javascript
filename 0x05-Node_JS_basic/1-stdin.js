8c69ca9d873c6fc0b10e3d42764d02038f3ebd7f
noahwekesa <noahwekesa0@gmail.com>
2024-07-19 22:46:12 +0300

    h

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,29 +1,15 @@
-const process = require('process');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdin.resume();
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-function askName() {
-  readline.question(
-    'Welcome to Holberton School, what is your name? \n',
-    (name) => {
-      console.log(`Your name is: ${name}`);
-      checkExit(name);
-    },
-  );
-}
-
-function checkExit(name) {
-  if (name.toLowerCase() === 'exit') {
-    console.log('This important software is now closing\n');
-    readline.close();
-  } else if (!process.stdin.isTTY) {
-    readline.close();
-  } else {
-    askName();
-  }
-}
-
-askName();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
